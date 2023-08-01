1f89de5f72efd5157def78d3f208de18ef1ea63d
Raphael Martins <raphael.m.nascimento6@aluno.senai.br>
2023-08-01 11:49:01 -0300

    atividades feitas

--- ex_fixacao02.py
+++ ex_fixacao02.py
@@ -1,9 +1,3 @@
-def calcular_conta_com_garcom(valor_conta):
-    gorjeta = valor_conta * 0.10
-    total_conta_com_garcom = valor_conta + gorjeta
-    return total_conta_com_garcom
+valor = int(input("Valor da compra:"))
 
-valor_conta = float(input("Digite o valor da conta: "))
-valor_total_com_garcom = calcular_conta_com_garcom(valor_conta)
-
-print(f"O valor total da conta com 10% do garçom é: R$ {valor_total_com_garcom:.2f}")
+print("O valor total da compra", (valor +(valor * 0.10 )))
